fe8826dded8cc63eeffe1ed447a913ec53629775
IvanOmelchenkoIP <io25112002@gmail.com>
2022-12-24 16:45:52 +0200

    Add predefined commands
    Add predefined commands in execute function

--- Course-1/Marked-Exercises-2/task-1/wish.h
+++ Course-1/Marked-Exercises-2/task-1/wish.h
@@ -9,6 +9,9 @@
 int batch_parse(char *argv[]);
 int prompt_parse();
 
+int path_ind = 1;
+char *paths[BUFF_SIZE] = {"/bin"};
+
 int main(int argc, char *argv[]) {
   // YOUR CODE HERE
   int return_code;
@@ -56,11 +59,60 @@ int prompt_parse() {
       free(buffer);
       return 0;
     }
-    free(buffer);
+    //free(buffer);
 
-    //char *args[] = parseInput(buffer);
+    int *args = parseInput(buffer);
+    for (int i = 0; i < 2; i++) printf("a1 %s\n", *args[1]);
+    /**/
 
-    if (access("/bin/ls", X_OK) != 0) {
+  }
+}
+
+
+void *parseInput(void *arg) {
+  char *input = strdup(arg);
+  char *chunk;
+  char *args_init[BUFF_SIZE];
+  int counter = 0;
+  while((chunk = strsep(&input, " ")) != NULL) {
+    char c = chunk[0];
+    if (isspace(c) != 0) continue;
+    printf("%s\n", chunk);
+    args_init[counter++] = *chunk;
+    //free(chunk);
+    //printf("%d\n", counter);
+  }
+  int args[counter];
+  for (int i = 0; i < counter; i++) {
+    args[i] = args_init[i];
+    printf("%s %s\n", *args_init[i], *args[i]);
+  }
+  return &args;
+};
+
+void executeCommands(char *args[], int args_num, FILE *out) {
+  if (strcmp(args[0], "exit") == 0) exit(0);
+  if (strcmp(args[0], "cd") == 2) {
+    if (args_num > 1) {
+      int status = chdir(args[1]);
+      if (status) {
+        printError();
+        exit(0);
+      }
+    } else {
+      printError();
+      exit(1);
+    }
+  }
+  if (strcmp(args[0], "path") {
+    for (int i = 1; i < args_num; i++) {
+      paths[path_ind++] = args[i];
+    }
+  }
+  if (strcmp(args[0]))
+
+
+  if (access(args[0], X_OK) != 0) {
       printf("1\n");
       printError();
       return 1;
@@ -72,37 +124,14 @@ int prompt_parse() {
       args[1] = "-la";
       int status = execv(args[0], args);
       printf("%d\n", status);
-      /*if (status) {
+      if (status) {
         printError();
-        return 1;
-      } */
+        exit(1);
+      }
     }else if (pid > 0) {
       wait(NULL);
     } else {
       printError();
-      return 1;
+      exit(1);
     }
-
-  }
-}
-
-
-void *parseInput(void *arg) {
-  char *input = strdup(arg);
-  char *chunk;
-  char *args[strlen(input)];
-  int counter = 0;
-  while((chunk = strsep(&input, " ")) != NULL) {
-    char c = chunk[0];
-    if (isspace(c) != 0) continue;
-    printf("%s\n", chunk);
-    counter++;
-    args[counter] = chunk;
-  }
-  return *args;
-};
-
-void executeCommands(char *args[], int args_num, FILE *out) {
-  int pid = fork();
-  printf("%d", pid);
 }
