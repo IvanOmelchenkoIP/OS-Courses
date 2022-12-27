e427e5789bab1f8d059457bebc055268e0568e89
IvanOmelchenkoIP <io25112002@gmail.com>
2022-12-27 14:11:31 +0200

    Fix task 1
    Fix task 1 by replacing scanf with a better one and assigning head value
    in task

--- Course-4/Marked-Exercises-1/task-1/main.c
+++ Course-4/Marked-Exercises-1/task-1/main.c
@@ -5,19 +5,19 @@
 
 int main(int argc, char *argv[])
 {
-    // YOUR CODE HERE
-    int sum, head = 0;
-    int sectors[SECTOR_NUM];
-    printf("Enter disk locations to service: ");
-    scanf("%d %d %d %d %d %d %d %d %d %d",
-    &sectors[0], &sectors[1], &sectors[2], &sectors[3], &sectors[4],
-    &sectors[5], &sectors[6], &sectors[7], &sectors[8], &sectors[9]);
-    for (int i = 0; i < SECTOR_NUM; i++) {
-        int cur_sector = sectors[i];
-        printf("Sector %d\n", cur_sector);
-        sum += abs(cur_sector - head);
-        head = cur_sector;
-    }
-    printf("Total Disk Head Movement: %d\n", sum);
-    return 0;
+  // YOUR CODE HERE
+  int sum;
+  int head = 10;
+  int sectors[SECTOR_NUM];
+  printf("Enter disk locations to service: ");
+  for (int i = 0; i < SECTOR_NUM; i++) scanf("%d", &sectors[i]);
+
+  for (int i = 0; i < SECTOR_NUM; i++) {
+    int cur_sector = sectors[i];
+    printf("Sector %d\n", cur_sector);
+    sum += abs(cur_sector - head);
+    head = cur_sector;
+  }
+  printf("Total Disk Head Movement: %d\n", sum);
+  return 0;
 }
