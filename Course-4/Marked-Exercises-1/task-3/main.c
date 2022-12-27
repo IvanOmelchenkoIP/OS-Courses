03e14fcc1e6e983100613fcad1161583d913e757
IvanOmelchenkoIP <io25112002@gmail.com>
2022-12-27 14:43:33 +0200

    Fix task 3
    Fix segmentation fail

--- Course-4/Marked-Exercises-1/task-3/main.c
+++ Course-4/Marked-Exercises-1/task-3/main.c
@@ -5,60 +5,42 @@
 
 int main(int argc, char *argv[])
 {
-    // YOUR CODE HERE
-    int left_num, right_num;
-    int left[SECTOR_NUM], right[SECTOR_NUM];
-    int sum, head = 10;
-    int sectors[SECTOR_NUM];
-    printf("Enter disk locations to service: ");
-    for (int i = 0; i < SECTOR_NUM; i++) scanf("%d", &sectors[i]);
+  // YOUR CODE HERE
+  int sum = 0;
+  int head = 10;
+  int sectors[SECTOR_NUM];
+  printf("Enter disk locations to service: ");
+  for (int i = 0; i < SECTOR_NUM; i++) scanf("%d", &sectors[i]);
 
-    for (int i = 0; i < SECTOR_NUM; i++)
-    {
-      if (sectors[i] < head) left[left_num++] = sectors[i];
-      else if (sectors[i] > head) right[right_num++] = sectors[i];
-    }
-
-    for (int i = 0; i < left_num; ++i)
-    {
-        for (int j = i + 1; j < left_num; ++j)
-        {
-            if (left[i] > left[j])
-            {
-                int tmp =  left[i];
-                left[i] = left[j];
-                left[j] = tmp;
-
-            }
-        }
+  for (int i = 0; i < SECTOR_NUM; i++) {
+    for (int j = 0; j < SECTOR_NUM - i - 1; j++) {
+      if (sectors[j] > sectors[j + 1]) {
+        int temp = sectors[j];
+        sectors[j] = sectors[j + 1];
+        sectors[j + 1] = temp;
       }
-
-      for (int i = 0; i < right_num; ++i)
-    {
-        for (int j = i + 1; j < right_num; ++j)
-        {
-            if (right[i] > right[j])
-            {
-                int tmp =  right[i];
-                right[i] = right[j];
-                right[j] = tmp;
-
-            }
-        }
-      }
-
-    for (int i = 0; i < right_num; i++) {
-        int cur_sector = right[i];
-        printf("Sector %d\n", cur_sector);
-        sum += abs(cur_sector - head);
-        head = cur_sector;
     }
-    for (int i = 0; i < left_num; i++) {
-        int cur_sector = right[i];
-        printf("Sector %d\n", cur_sector);
-        sum += abs(cur_sector - head);
-        head = cur_sector;
+  }
+
+  int head_index;
+  for (int i = 0; i < SECTOR_NUM; i++) {
+    if (head < sectors[i]) {
+      head_index = i;
+      break;
     }
-    printf("Total Disk Head Movement: %d\n", sum);
-    return 0;
+  }
+  for (int i = head_index; i < SECTOR_NUM; i++) {
+    int cur_sector = sectors[i];
+    printf("Sector %d\n", cur_sector);
+    sum += abs(cur_sector - head);
+    head = cur_sector;
+  }
+  for (int i = 0; i < head_index; i++) {
+    int cur_sector = sectors[i];
+    printf("Sector %d\n", cur_sector);
+    sum += abs(cur_sector - head);
+    head = cur_sector;
+  }
+  printf("Total Disk Head Movement: %d\n", sum);
+  return 0;
 }
