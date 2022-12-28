8934241092b5584c776baabef255f159aa3cac07
IvanOmelchenkoIP <io25112002@gmail.com>
2022-12-28 19:56:02 +0200

    Updates
    Updates to the code

--- Course-4/Marked-Exercises-4/task-1/UDPClient.c
+++ Course-4/Marked-Exercises-4/task-1/UDPClient.c
@@ -12,37 +12,35 @@
 
 int main() {
 
-  // YOUR CODE HERE
-  int socket_id;
-  char *buffer[MAXLINE];
-  struct sockaddr_in client, server;
-  memset(&client, 0, sizeof(client));
-  memset(&server, 0, sizeof(server));
+	// YOUR CODE HERE
+	int socket_descriptor;
+	char *buffer[MAXLINE];
+	struct sockaddr_in client, server;
+	memset(&client, 0, sizeof(client));
+	memset(&server, 0, sizeof(server));
 
-  server.sin_family = AF_INET;
+	server.sin_family = AF_INET;
   server.sin_addr.s_addr = INADDR_ANY;
   server.sin_port = htons(PORT);
 
-  if (socket_id = socket(AF_INET, SOCK_DGRAM, 0) < 0) {
-    printf("Error creating socket...");
-    return 1;
-  }
+	if (socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0) < 0) {
+		printf("Error creating socket...\n");
+		return 1;
+	}
 
-  if (bind(socket_id, (const struct sockaddr *)&server, sizeof(server)) < 0) {
-    printf("Error binding to server...");
-    return 1;
-  }
+	/*if (*/bind(socket_descriptor, (const struct sockaddr *)&server, sizeof(server)); /*< 0) {
+    printf("Error binding to server...\n");
+		return 1;
+  } */
 
-  int size = recvfrom(socket_id, (char *)buffer, MAXLINE,
-                MSG_WAITALL, (struct sockaddr *) &client, &sizeof(client));
+  int size = (int)recvfrom(socket_descriptor, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&client, (socklen_t *)sizeof(client));
   buffer[size] = '\0';
-  printf("Client Says: %s\n", buffer);
+  printf("Client Says: %s\n", *buffer);
 
- char *server_message = "I am file, thank you."
-  sendto(socket_id, (const char *)server_message, strlen(server_message),
+	char *server_message = "I am fine, thank you.";
+  sendto(socket_descriptor, (const char *)server_message, strlen(server_message),
         MSG_CONFIRM, (const struct sockaddr *) &client, sizeof(client));
   printf("Response sent.\n");
 
-  return 0;
+	return 0;
 }
-
--- Course-4/Marked-Exercises-4/task-1/UDPServer.c
+++ Course-4/Marked-Exercises-4/task-1/UDPServer.c
@@ -13,34 +13,27 @@
 int main() {
 
 	// YOUR CODE HERE
-	int socket_id;
+	int socket_descriptor;
 	char *buffer[MAXLINE];
-	struct sockaddr_in, server;
+	struct sockaddr_in server;
 	memset(&server, 0, sizeof(server));
 
 	server.sin_family = AF_INET;
   server.sin_addr.s_addr = INADDR_ANY;
   server.sin_port = htons(PORT);
 
-	if (socket_id = socket(AF_INET, SOCK_DGRAM, 0) < 0) {
-		printf("Error creating socket...");
+	if (socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0) < 0) {
+		printf("Error creating socket...\n");
 		return 1;
 	}
 
-	if (bind(socket_id, (const struct sockaddr *)&server, sizeof(server)) < 0) {
-    printf("Error binding to server...");
-		return 1;
-  }
-
-	char *client_message = "Hello, how are you?"
-  sendto(socket_id, (const char *)client_message, strlen(client_message),
-        MSG_CONFIRM, (const struct sockaddr *) &client, sizeof(client));
+	char *client_message = "Hello, how are you?";
+  sendto(socket_descriptor, (const char *)client_message, strlen(client_message), MSG_CONFIRM, (const struct sockaddr *)&server, sizeof(server));
   printf("Hello message sent.\n");
 
-  int size = recvfrom(socket_id, (char *)buffer, MAXLINE,
-                MSG_WAITALL, (struct sockaddr *) &server, &sizeof(server));
+  int size = recvfrom(socket_descriptor, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&server, (socklen_t *)sizeof(server));
   buffer[size] = '\0';
-  printf("Client Says: %s\n", buffer);
+  printf("Client Says: %s\n", *buffer);
 
 	return 0;
 }
